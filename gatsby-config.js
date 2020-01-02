e076f1414d6a23024194a458c0cfc67e8af0e035
JuanJavier1979 <jjmrestituto@gmail.com>
2020-01-02 17:31:33 +0100

    [Add] Basic plugins that will be used [Add] Basic Gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,47 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: 'Gatsby Challenge - Static Blog',
+    author: '@JuanJavier1979'
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-remark-relative-images'
+        ]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+          fonts: [
+              {
+                  family: `Noto+Serif`,
+                  variants: [`400`, `400i`, `700`]
+              },
+              {
+                  family: `Amiri`,
+                  variants: [`400`, `700`]
+              },
+              {
+                  family: `Lato`,
+                  variants: [`400`, `700`]
+              }
+          ],
+      },
+  },
+  ]
 }
