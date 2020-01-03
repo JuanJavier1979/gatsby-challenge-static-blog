74dd2f537f2bff4429cbb41095c35a09585d23aa
JuanJavier1979 <jjmrestituto@gmail.com>
2020-01-03 15:58:37 +0100

    [Add] TailwindCSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 import React from "react"
+import '../styles/global.css'
 
-export default () => <div>Hello world!</div>
+export default () => <div className="container mx-auto p-6 text-center">Hello world!</div>
