1375b8f2347c716ec46336511ad514856abe68ee
JuanJavier1979 <jjmrestituto@gmail.com>
2020-04-06 12:04:05 +0200

    Refactor homepage’s hero area to a CSS grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,15 +15,11 @@ class IndexPage extends React.Component {
     return (
       <Layout>
         <SEO title="Home" />
-        <section className="container mx-auto p-6 flex flex-wrap flex-col md:flex-row items-center">
-          <div className="flex flex-col w-full md:w-1/2 justify-center items-start md:text-left pr-8">
-            <h1 className="font-serif font-bold text-4xl sm:text-5xl md:text-5xl xl:text-6xl leading-none">Welcome<br />to Juan Javier's Blog</h1>
-            <p className="mt-6">This is the first version of my blog. <br />I am starting the year with a development challenge. In the first posts I will be writing about the process.<br />Let's start the year with some Gatsby coding!</p>
-          </div>
-          <div className="flex flex-col w-full md:w-1/2 justify-center items-start text-center md:text-left">
-            <img alt="Cat and human sitting on a couch" className="block mx-auto" src={catAndHumanIllustration} />
-          </div>
-          </section>
+        <section className="container mx-auto p-6 hero-area">
+          <h1 className="font-serif font-bold text-4xl sm:text-5xl md:text-5xl xl:text-6xl leading-none">Welcome<br />to Juan Javier's Blog</h1>
+          <p className="my-8 md:m-0">This is the first version of my blog. <br />I am starting the year with a development challenge. In the first posts I will be writing about the process.<br />Let's start the year with some Gatsby coding!</p>
+          <img alt="Cat and human sitting on a couch" className="block mx-auto" src={catAndHumanIllustration} />
+        </section>
         <section className="container mx-auto p-6 posts-container">
           <h2 className="text-3xl font-serif font-black mb-2">Read all posts</h2>
           {posts.map(({ node }) => {
